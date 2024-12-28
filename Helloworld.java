e612bc00c4c640b50dd91d9b83e46668620a3314
Vaishnavi Nikumbh <vaishnavi.official30@gmail.com>
2024-12-28 19:49:50 +0530

    Change Updated

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
 class Helloworld{
     public static void main(String args[]){
         System.out.println("Hello World Program: Write a Java program to print Hello, World");
-        System.out.println("Hello World");
+        System.out.println("HELLO JAVA");
 
     }
 }
